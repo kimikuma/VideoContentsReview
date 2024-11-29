590c8a686e1e37158d00aa8b78b776f9819efe4a
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-11-29 20:19:22 +0900

    アプリケーション設計

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "jquery";
-import "popper.js";
-import "bootstrap";
-import "../stylesheets/application";
+
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import '../stylesheets/application'
+import '../stylesheets/mystylesheet.css'
