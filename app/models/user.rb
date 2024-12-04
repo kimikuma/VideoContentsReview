56a0a2676a6143b4d90f4d808ac759ba224e73c6
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-12-04 07:31:51 +0000

    実装フェーズ１

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,34 @@
 class User < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+  has_many :posts, dependent: :destroy
+
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [ width, height ]).processed
+  end
+       
+  GUEST_USER_EMAIL="guest@sample.com"
+       
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+     user.password=SecureRandom.urlsafe_base64
+     user.name="guest_uesr"
+    end
+  end
+       
+  def guest_user?
+    email==GUEST_USER_EMAIL
+  end
 end
--- app/models/vod_item.rb
+++ app/models/vod_item.rb
@@ -1,4 +1,6 @@
 class VodItem < ApplicationRecord
   belongs_to :post
   belongs_to :vod
+
+  validates :vod_id, uniqueness: { scope: :post_id}
 end
