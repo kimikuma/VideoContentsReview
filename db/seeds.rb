983b6ccd3ebddba1a7f6f63d3c379388372e81ee
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-12-05 11:34:35 +0000

    seedテストデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Genre.find_or_create_by!(name: "邦画")
+Genre.find_or_create_by!(name: "洋画")
+Vod.find_or_create_by!(name: "Video")
+Vod.find_or_create_by!(name: "Drama")
+
+
