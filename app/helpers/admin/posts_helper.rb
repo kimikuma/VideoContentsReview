771581f1727022fb409565813fd4e227d73c3a89
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-12-11 05:47:33 +0000

    タグ機能完成

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -1,2 +1,10 @@
 module Admin::PostsHelper
+
+  def display_tag_items_edit(tag_items)
+    tag_items.map { |item| link_to("##{item.tag.name}", edit_admin_tag_path(item.tag.id)) }.join('').html_safe
+  end 
+  
+  def display_vod_items_edit(vod_items)
+    vod_items.map { |item| link_to(item.vod.name, edit_admin_vod_path(item.vod.id)) }.join(',').html_safe
+  end   
 end
