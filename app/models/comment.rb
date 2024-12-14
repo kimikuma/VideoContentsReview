05ee0cd1569864655dac1a4da5849c5f6c3a5c67
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-12-14 04:43:55 +0000

    通知機能

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  has_many :notifications, as: :notifiable, dependent: :destroy
 
   validates :comment, presence: true, length: {maximum: 30}
+
+  after_create do 
+    notifications.create(user_id: post.user_id)
+  end  
 end
