bffdd71ff79ec59f04d833d6d4987342aabd629d
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-12-21 06:10:19 +0000

    レイアウト調整

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many :tag_items, dependent: :destroy
   has_many :posts, through: :tag_items
   
-  validates :name, uniqueness: true, length: {maximum: 30}
+  validates :name, presence: true, uniqueness: true, length: {maximum: 30}
 end
